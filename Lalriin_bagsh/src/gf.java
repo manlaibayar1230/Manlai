0116c00fc046a5097f69766bd1ce1eee4e7878bd
admin <admin@DESKTOP-M3C79NP>
2024-02-06 15:26:12 +0800

    bi ynzlla

--- Lalriin_bagsh/src/gf.java
+++ Lalriin_bagsh/src/gf.java
@@ -1,4 +1,22 @@
 
 public class gf {
+	
 
-}
+
+	 private static final double PI = 3.14;
+	 private final double radius;
+	 public Circle(double radius) {
+	 this.radius = radius;
+	 }
+	 public static void main(String[] args) {
+	 System.out.println("------------------------------------------------------------");
+	 System.out.println("CircleCalc v1.0");
+	 System.out.println();
+	 System.out.println("Calculates and prints information for a user-supplied radius");
+	 System.out.println("------------------------------------------------------------");
+	 System.out.print("Enter the circle's radius: ");
+	 Scanner in = new Scanner(System.in);
+	 Circle c = new Circle(in.nextFloat());
+	 // TODO: compute and print the circumference and area of the circle
+	 }
+	}
